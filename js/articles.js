83216882e9c076f4a792a2136eeedc1b9fa692c3
Sasha <69010894+ArmSasha@users.noreply.github.com>
2024-07-04 16:48:32 +0400

    Update articles.js

--- js/articles.js
+++ js/articles.js
@@ -20,20 +20,38 @@ function closeSettings(){
 // Dark mode
 
 let styleMode = localStorage.getItem('styleMode');
+const themeToggle = document.getElementById('themeToggle');
 
-
+const enableDarkStyle = () => {
+    document.body.setAttribute('dark', '')
+    localStorage.setItem('styleMode', 'dark');
+    themeToggle.checked = true;
+}
+const disableDarkStyle = () => {
+    document.body.removeAttribute('dark');
+    localStorage.setItem('styleMode', 'light');
+    themeToggle.checked = false;
+}
 
 const ChangeTheme = (isChecked) => {
     if (isChecked) {
-        document.body.setAttribute('dark', '')
+        enableDarkStyle()
     } else{
-        document.body.removeAttribute('dark')
+        disableDarkStyle()
     }
 }
 
+if (styleMode === 'dark') {
+    enableDarkStyle();
+}
+
+themeToggle.addEventListener('change', (event) => {
+    ChangeTheme(event.target.checked);
+});
 
 
 //______________________________________________________________________________________________________
+
 // Swiper
 
 var swiper = new Swiper(".mySwiper", {
