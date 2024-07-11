2e8c0a223792303755e8fa1ab2902e2569a73f35
Sasha <69010894+ArmSasha@users.noreply.github.com>
2024-07-11 14:15:33 +0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,6 +8,8 @@ const tg = window.Telegram.WebApp;
 
 //Popups
 
+// Settings: 
+
 let settingsPopup = document.getElementById("settingsPopup");
 
 function openSettings(){
@@ -16,6 +18,107 @@ function openSettings(){
 function closeSettings(){
     settingsPopup.classList.remove("open-popup")
 }
+
+//______________________________________________________________________________________________________
+
+// Mark Period: 
+
+let MarkPeriodPopup = document.getElementById('calendar');
+
+function OpenMarkPeriod() {
+    MarkPeriodPopup.classList.add('open-mark');
+}
+function CloseMarkPeriod(){
+    MarkPeriodPopup.classList.remove("open-mark");
+}
+
+
+let calendar = document.querySelector('.calendar')
+
+const month_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+isLeapYear = (year) => {
+    return (year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) || (year % 100 === 0 && year % 400 ===0)
+}
+
+getFebDays = (year) => {
+    return isLeapYear(year) ? 29 : 28
+}
+
+generateCalendar = (month, year) => {
+
+    let calendar_days = calendar.querySelector('.calendar-days')
+    let calendar_header_year = calendar.querySelector('#year')
+
+    let days_of_month = [31, getFebDays(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    calendar_days.innerHTML = ''
+
+    let currDate = new Date()
+    if (!month) month = currDate.getMonth()
+    if (!year) year = currDate.getFullYear()
+
+    let curr_month = `${month_names[month]}`
+    month_picker.innerHTML = curr_month
+    calendar_header_year.innerHTML = year
+
+    // get first day of month
+    
+    let first_day = new Date(year, month, 1)
+
+    for (let i = 0; i <= days_of_month[month] + first_day.getDay() - 1; i++) {
+        let day = document.createElement('div')
+        if (i >= first_day.getDay()) {
+            day.classList.add('calendar-day-hover')
+            day.innerHTML = i - first_day.getDay() + 1
+            day.innerHTML += `<span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>`
+            if (i - first_day.getDay() + 1 === currDate.getDate() && year === currDate.getFullYear() && month === currDate.getMonth()) {
+                day.classList.add('curr-date')
+            }
+        }
+        calendar_days.appendChild(day)
+    }
+}
+
+let month_list = calendar.querySelector('.month-list')
+
+month_names.forEach((e, index) => {
+    let month = document.createElement('div')
+    month.innerHTML = `<div data-month="${index}">${e}</div>`
+    month.querySelector('div').onclick = () => {
+        month_list.classList.remove('show')
+        curr_month.value = index
+        generateCalendar(index, curr_year.value)
+    }
+    month_list.appendChild(month)
+})
+
+let month_picker = calendar.querySelector('#month-picker')
+
+month_picker.onclick = () => {
+    month_list.classList.add('show')
+}
+
+let currDate = new Date()
+
+let curr_month = {value: currDate.getMonth()}
+let curr_year = {value: currDate.getFullYear()}
+
+generateCalendar(curr_month.value, curr_year.value)
+
+document.querySelector('#prev-year').onclick = () => {
+    --curr_year.value
+    generateCalendar(curr_month.value, curr_year.value)
+}
+
+document.querySelector('#next-year').onclick = () => {
+    ++curr_year.value
+    generateCalendar(curr_month.value, curr_year.value)
+}
+
 
 
 //______________________________________________________________________________________________________
@@ -57,10 +160,39 @@ themeToggle.addEventListener('change', (event) => {
 
 // USER INFO
 
-// const user_info_block = document.getElementById("user-info");
-// const user_info = window.Telegram.WebApp.initDataUnsafe;
+const user_info_block = document.getElementById("user-info");
+const user_info = window.Telegram.WebApp.initDataUnsafe;
 
-// function get_user_info(user_info) {
+function get_user_info(user_info) {
+        if (user_info) {
+            user_info_block.innerHTML = `
+                ID: ${user_info.id || "Не указано"} <br>
+                Имя: ${user_info.first_name || "Не указано"} <br>
+                Фамилия: ${user_info.last_name || "Не указано"} <br>
+                Username: ${user_info.username || "Не указано"} <br>
+                Язык: ${user_info.language_code || "Не указано"} <br>
+                Премиум: ${user_info.is_premium ? "Да" : "Нет"} <br>
+                Фото: ${user_info.photo_url || "Нет фото"}`;
+        } else {
+            user_info_block.innerHTML = "Данные пользователя не найдены.";
+        }
+    };
+
+// console.log(get_user_info(user_info.user));
+get_user_info(user_info.user);
+
+
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     // USER INFO
+    
+//     const user_info_block = document.getElementById("user-info");
+    
+//     // Логирование данных initDataUnsafe
+//     console.log("initDataUnsafe:", window.Telegram.WebApp.initDataUnsafe);
+    
+//     const initDataUnsafe = window.Telegram.WebApp.initDataUnsafe;
+    
+//     function get_user_info(user_info) {
 //         if (user_info) {
 //             user_info_block.innerHTML = `
 //                 ID: ${user_info.id || "Не указано"} <br>
@@ -74,44 +206,15 @@ themeToggle.addEventListener('change', (event) => {
 //             user_info_block.innerHTML = "Данные пользователя не найдены.";
 //         }
 //     };
-
-// get_user_info(user_info.user);
-
-
-document.addEventListener('DOMContentLoaded', (event) => {
-    // USER INFO
-    const user_info_block = document.getElementById("user-info");
-
-    // Логирование данных initDataUnsafe
-    console.log("initDataUnsafe:", window.Telegram.WebApp.initDataUnsafe);
-
-    const initDataUnsafe = window.Telegram.WebApp.initDataUnsafe;
-
-    function get_user_info(user_info) {
-        console.log("User info object:", user_info);
-        if (user_info) {
-            user_info_block.innerHTML = `
-                ID: ${user_info.id || "Не указано"} <br>
-                Имя: ${user_info.first_name || "Не указано"} <br>
-                Фамилия: ${user_info.last_name || "Не указано"} <br>
-                Username: ${user_info.username || "Не указано"} <br>
-                Язык: ${user_info.language_code || "Не указано"} <br>
-                Премиум: ${user_info.is_premium ? "Да" : "Нет"} <br>
-                Фото: ${user_info.photo_url ? `<img src="${user_info.photo_url}" alt="Фото пользователя">` : "Нет фото"}`;
-        } else {
-            user_info_block.innerHTML = "Данные пользователя не найдены.";
-        }
-    };
-
-    if (initDataUnsafe && initDataUnsafe.user) {
-        console.log("Данные пользователя:", initDataUnsafe.user);
-        get_user_info(initDataUnsafe.user);
-    } else {
-        console.log("initDataUnsafe или initDataUnsafe.user не определены");
-        user_info_block.innerHTML = "Ошибка получения данных пользователя.";
-    }
-});
-
+    
+//     if (initDataUnsafe && initDataUnsafe.user) {
+//         console.log("Данные пользователя:", initDataUnsafe.user);
+//         get_user_info(initDataUnsafe.user);
+//     } else {
+//         console.log("initDataUnsafe или initDataUnsafe.user не определены");
+//         user_info_block.innerHTML = "Ошибка получения данных пользователя.";
+//     }
+// });
 
 
   
