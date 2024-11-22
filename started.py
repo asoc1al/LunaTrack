a28a7ef565d5a05558453b0cc88b2699149c278c
Sasha Badikyan <sadaken228@gmail.com>
2024-11-22 09:29:51 +0400

    стартер бота

--- started.py
+++ started.py
@@ -1,24 +1,27 @@
-# from aiogram.utils import executor
 import logging
-from aiogram import Bot
-from aiogram import Dispatcher
-from aiogram.fsm.storage.memory import MemoryStorage
-
+from aiogram import Bot, Dispatcher
+import asyncio
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token='6323930950:AAEipDRJLbCuIRmZqq0HNlatXAJGlsgNac8')
-# storage = MemoryStorage()
+# Создание объекта бота
+bot = Bot(token='5760615918:AAHygCHZdAnKceDwVIBzhT_L-INrrbK6HbI')
+
+# Создание диспетчера без передачи бота
 dp = Dispatcher()
 
 
+async def on_startup():
+    print('Бот вышел в онлайн')
+    await bot.send_message(1030874842, 'Online')
 
-async def on_startup(_):
-	print('Бот вышел в онлайн')
-	await bot.send_message(1030874842, 'Online')
 
+async def main():
+    # Привязка объекта бота к диспетчеру
+    dp["bot"] = bot
+    await on_startup()
+    await dp.start_polling(bot, skip_updates=True)
 
 
 if __name__ == '__main__':
-    dp.start_polling(dp, skip_updates = True)
-
+    asyncio.run(main())
